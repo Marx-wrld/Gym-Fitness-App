04f7e655828a444d2882239aec4a584de598a252
Marx-wrld <iongonga@kabarak.ac.ke>
2022-12-26 13:00:50 +0300

    fetching images and icons and setting the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from "@mui/material"; // A box is basically a div with some shading and colors
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 function App() {
   return (
-   <h1>Hello world</h1>
-  );
+   <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/exercise/:id" element={<ExerciseDetail/>}/> {/*which means that the id is going to be dynamic*/}
+    </Routes>
+    <Footer/>
+   </Box>
+  )
 }
 
 export default App;
