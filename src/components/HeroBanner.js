770dc3b89378fa5b5c78892a2de210fdd4bcfc5e
Marx-wrld <iongonga@kabarak.ac.ke>
2022-12-27 17:46:00 +0300

    HeroBanner, SearchExercises and Exercises sections

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material'; //typography is just an element like <p> tag with additional styles
+
+const HeroBanner = () => {
+  return (
+    <Box>
+        <Typography>
+            
+        </Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner
