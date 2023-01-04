328acc0369ebe6b53e65fccf281a7b4f9df86488
Ian Marx <105711066+Marx-wrld@users.noreply.github.com>
2023-01-04 20:51:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-   <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+   <Box width="400px" 
+    sx={{ 
+       width: { xl: '1488px' }
+       }} 
+       m="auto">
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
