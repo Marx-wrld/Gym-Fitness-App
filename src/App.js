f84b4c1e03bc7bf4b1b8b9231607c4cebd5ac8b9
Marx-wrld <iongonga@kabarak.ac.ke>
2023-01-05 17:59:55 +0300

    resolving api key bug

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from "@mui/material"; // A box is basically a div with some shading and colors
@@ -16,7 +17,7 @@ function App() {
     </Routes>
     <Footer/>
    </Box>
-  )
+  );
 }
 
 export default App;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,13 +2,13 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
 
 
-export const fetchData = async (url, options) => { //urls and addtional options parameters
-    const response = await fetch(url, options); //fetching the data
+export const fetchData = async (urls, options) => { //urls and addtional options parameters
+    const response = await fetch(urls, options); //fetching the data
     const data = await response.json(); //extracting the data
 
     return data;
