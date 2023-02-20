39845e80245960980be890058f167a8c406b0925
Ian Marx <105711066+Marx-wrld@users.noreply.github.com>
2023-02-20 23:51:23 +0300

    Update HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -9,22 +9,22 @@ const HeroBanner = () => {
     mt: { lg: '212px', xs: '70px'},
     ml: { sm: '50px'}
   }} position="relative" p="20px">
-        <img src={HeroBannerImage} alt="banner" className="hero-banner-img" style={{float: "right", marginBottom: "10px"}}/>
-        <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" style={{float: "right", marginBottom: "10px"}}/>
+       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
           Fitness Club
-        </Typography>
-        <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px'}}} mb="23px" mt="30px">
+       </Typography>
+       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px'}}} mb="23px" mt="30px">
           Sweat, Smile <br/> Repeat!
-        </Typography>
-        <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px" mb={3}>
+       </Typography>
+       <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px" mb={3}>
           Check out the Most Effective Exercises
-        </Typography>
-        <Button variant="contained" color="error" href="#exercises" sx={{ backgroundColor: '#FF2625', padding: '10px'}}>
+       </Typography>
+       <Button variant="contained" color="error" href="#exercises" sx={{ backgroundColor: '#FF2625', padding: '10px'}}>
           Explore Exercises
-        </Button>
-        <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+       </Button>
+       <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
       Exercise
-        </Typography>
+       </Typography>
   </Box>
   );
 }
