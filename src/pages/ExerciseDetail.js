35d282e4a35f6c466d48081b20ec95cc79618279
Marx-wrld <iongonga@kabarak.ac.ke>
2023-03-07 13:11:54 +0300

    ExerciseDetail  build

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import {useParams} from 'react-router-dom'
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseCard';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async() => {
+
+    }
+
+    fetchExercisesData();
+  }, [id]);
   return (
-    <div>
-      ExerciseDetail
-    </div>
+    <Box>
+      <Detail exerciseDetail={}/>
+      <ExerciseVideos />
+      <SimilarExercises/>
+    </Box>
   )
 }
 
