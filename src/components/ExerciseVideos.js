972e2b705a4e82dd90e31c8d8428c66e8dc7758a
Marx-wrld <iongonga@kabarak.ac.ke>
2023-03-08 10:59:26 +0300

    ExerciseVideos component

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-const ExerciseVideos = () => {
+
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  
+  if (exerciseVideos.length) return 'loading...';
+  
   return (
-    <div>
-      
-    </div>
+    <Box 
+    sx={{
+      marginTop: { lg: '200px', xs: '20px'}
+    }}
+    p="20px">
+      <Typography variant="h3" mb="33px">
+        Watch<span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> of exercise videos
+      </Typography>
+      <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center"
+      sx={{
+        flexDirection: {
+          lg: 'row'
+        },
+        gap: {
+          lg: '110px',
+          xs: '0'
+        }
+      }}>
+      {/*The question mark allows us to make sure the exerciseVideos exists. The slice allows us to decide o how many we want to take (0,3) means we are going to take three videos. We then map over it to get each item or a video*/}
+        {exerciseVideos?.slice(0, 3).map((item, index) => (
+          <a 
+          /*We use an anchor tag because each of our videos is going to be a link tom check it out on youtube*/
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`} 
+            target="_blank" /*Allows us open it in a new tab*/
+            rel="noreferrer" /*Recommended when using _blank*/
+            >
+              <img src={item.video.thumbnails[0].url} alt={item.video.title} /> 
+              {/*Getting this data frm rapidapi*/}
+              <Box>
+                <Typography variant="h5" color="#000">
+                  {item.video.title} 
+                </Typography>
+                <Typography variant="h6" color="#000">
+                  {item.video.channelName} 
+                </Typography>
+              </Box>
+          </a>
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,11 +1,18 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'fa215201c6msh00f0fb15220e24ap102a5ejsna2766eaea227',
+  },
+};
 
 export const fetchData = async (urls, options) => { //urls and addtional options parameters
     const response = await fetch(urls, options); //fetching the data
